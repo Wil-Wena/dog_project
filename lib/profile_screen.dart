088d2d4d84368384bce7c4579f57ceee2b144ceb
Wilson Aballey <willywallet001@gmail.com>
2023-09-05 19:21:53 +0000

    .

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -5,6 +5,93 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Image.asset('assets/walk.jpg'),
+            Transform.translate(
+              offset: Offset(0, 100),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: Column(
+                  children: [
+                    _buildProfileImage(context),
+                    _buildProfileDetails(context),
+                    _buildActions(context)
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+Widget _buildProfileImage(BuildContext context) {
+  return Container(
+    width: 200,
+    height: 200,
+    child: ClipOval(
+      child: Image.asset(
+        'assets/dog.jpg',
+        fit: BoxFit.fitWidth,
+      ),
+    ),
+  );
+}
+
+Widget _buildProfileDetails(BuildContext context) {
+  return Padding(
+    padding: EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Teddy American",
+          style: TextStyle(fontSize: 35, fontWeight: FontWeight.w600),
+        ),
+        _buildDetailsRow('Age', '4'),
+        _buildDetailsRow('Status', 'Good Boy')
+      ],
+    ),
+  );
+}
+
+Widget _buildDetailsRow(String heading, String value) {
+  return Row(
+    children: [
+      Text(
+        '$heading: ',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      Text(value)
+    ],
+  );
+}
+
+Widget _buildIcon(IconData icon, String text) {
+  return Padding(
+    padding: EdgeInsets.all(20),
+    child: Column(children: [
+      Icon(
+        icon,
+        size: 40,
+      ),
+      Text(text)
+    ]),
+  );
+}
+
+Widget _buildActions(BuildContext context) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      _buildIcon(Icons.restaurant, "Feed"),
+      _buildIcon(Icons.favorite, 'Pet'),
+      _buildIcon(Icons.directions_walk, 'Walk')
+    ],
+  );
 }
